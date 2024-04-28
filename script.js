a62874c24b37001eb22703b369b4ca879565edc7
PepsiColaS <workmail912@mail.ru>
2024-04-29 00:38:57 +0300

    'modified'

--- script.js
+++ script.js
@@ -1,47 +1,32 @@
-function loadNewPage(url) {
-    // Изменяем URL без перезагрузки страницы
-    window.history.pushState({page: "newPage"}, "New Page", url);
-    
-    // Загружаем новое содержимое страницы
-    fetch(url)
-        .then(response => response.text())
-        .then(data => {
-            document.body.innerHTML = data;
-            if (url == "timer.html"){
-               timer();
-            }
-            if (url == "map.html"){
-                initMap();          
-             }
-        });
-}
 
-
-// Создание янедкс карты
+// Создание яндекс карты
 
 function initMap() {
-    ymaps.ready(function() {
+    ymaps.ready(function () {
         document.querySelector('.preloader').style.display = 'block';
 
-            var myMap = new ymaps.Map("map", {
-                center: [56.740058, 37.225314],
-                zoom: 12
-            });  
-    
-            var myPlacemark = new ymaps.Placemark([56.740058, 37.225314], {
-                hintContent: 'Место проживания',
-                balloonContent: 'Место проживания'
-            });
-    
-            myMap.geoObjects.add(myPlacemark);
-            document.querySelector('.preloader').style.display = 'none'; 
-            
-            document.querySelector('.second-map').style.height = '100%'
+        var myMap = new ymaps.Map("map", {
+            center: [56.740058, 37.225314],
+            zoom: 12
+        });
+
+        var myPlacemark = new ymaps.Placemark([56.740058, 37.225314], {
+            hintContent: 'Место проживания',
+            balloonContent: 'Место проживания'
+        });
+
+        myMap.geoObjects.add(myPlacemark);
+        document.querySelector('.preloader').style.display = 'none';
+
+        document.querySelector('.second-map').style.height = '100%'
     });
 }
 
+
+
+
 // Таймер
-function timer(){
+function timer() {
     if (!sessionStorage.getItem('startTime')) {
         sessionStorage.setItem('startTime', Date.now());
     }
@@ -66,3 +51,51 @@ function timer(){
 
     setInterval(updateTimeSpent, 1000); // Обновляем каждую секунду
 }
+
+
+// SPA
+document.querySelectorAll('a').forEach(function (element) {
+    element.addEventListener('click', function (event) {
+        event.preventDefault();
+        var url = this.getAttribute('href');
+
+        history.pushState({ page: url }, '', url);
+        
+        if (url == '/index.html') url = '/activity.html'
+
+        loadPageContent(url);
+    });
+});
+
+window.addEventListener('popstate', function (event) {
+    if (event.state) {
+        var url = event.state.page;
+
+        if (url == '/index.html') url = '/activity.html'
+        loadPageContent(url);
+    }
+});
+
+function loadPageContent(url) {
+    fetch(url)
+    .then(response => response.text())  
+    .then(text => {
+        if (text.length) {
+            document.getElementById('container').innerHTML = text;
+            if (url == '/map.html') initMap();
+            if (url == '/timer.html') timer();
+        } else {
+            console.log('Error')
+        }
+    })
+    .catch(error => console.error('Ошибка при загрузке контента:', error));
+
+}
+
+window.onload = function() {
+    loadPageContent('/activity.html')
+};
+
+window.addEventListener('hashchange', function() {
+    console.log('fggffg')
+});
